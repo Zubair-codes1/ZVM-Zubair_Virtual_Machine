823105419e15cde386d5709caf6d2b762356dbf0
Zubair <zubairabdulmatin@gmail.com>
2026-05-17 22:50:04 +0100

    fixing file structure as project grows

--- vm/src/Main.java
+++ vm/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         VirtualMachine virtualMachine = new VirtualMachine();
-        virtualMachine.loadFromFile("src/test.txt");
+        virtualMachine.loadFromFile("vm/src/test.txt");
         virtualMachine.executeProgram();
         virtualMachine.returnStackData();
         virtualMachine.printGlobalVariables();
