3af5dda34e0af068d30e29f91d0a3019c8331488
Zubair <zubairabdulmatin@gmail.com>
2026-06-29 18:49:51 +0100

    Added CompilerException class to handle compiler exceptions

---
+++ compiler/src/CompilerException.java
@@ -0,0 +1,14 @@
+/**
+ * Class to handle all compiler exceptions
+ *
+ * @author Zubair Abdul Matin
+ */
+public class CompilerException extends RuntimeException {
+    /**
+     * Passes all compiler execeptions to the Runtime execption
+     * @param message error message
+     */
+    public CompilerException(String message) {
+        super(message);
+    }
+}
