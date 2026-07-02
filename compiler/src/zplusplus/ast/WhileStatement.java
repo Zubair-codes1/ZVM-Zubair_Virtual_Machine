fce95d739cb35ed18c153d2fd016af0783bc8725
Zubair <zubairabdulmatin@gmail.com>
2026-07-02 11:50:50 +0100

    Completed while statement node, includes a condition and body statements (as a block node)

--- compiler/src/zplusplus/ast/WhileStatement.java
+++ compiler/src/zplusplus/ast/WhileStatement.java
@@ -1,4 +1,29 @@
 package zplusplus.ast;
 
-public class WhileStatement {
+public class WhileStatement extends Statement {
+
+    public final Expression condition;
+    // likely a block statement
+    public final Statement body;
+
+    public WhileStatement(Expression condition, Statement body, int lineNumber) {
+        super(lineNumber);
+
+        this.condition = condition;
+        this.body = body;
+    }
+
+    public Expression getCondition() {
+        return condition;
+    }
+
+    public Statement getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+
+        return "while (" + condition.toString() + ") {\n" + body.toString() + "}\n";
+    }
 }
