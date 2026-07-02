639434724b3a5ddf8fac5fe070fc710ec2cdd5b0
Zubair <zubairabdulmatin@gmail.com>
2026-07-02 11:31:41 +0100

    Completed Assignment statement node for the AST

--- compiler/src/zplusplus/ast/AssigmentStatement.java
+++
@@ -1,4 +0,0 @@
-package zplusplus.ast;
-
-public class AssigmentStatement {
-}
---
+++ compiler/src/zplusplus/ast/AssignmentStatement.java
@@ -0,0 +1,26 @@
+package zplusplus.ast;
+
+public class AssignmentStatement extends Statement {
+
+    private final String name;  // name of variable
+    private final Expression expression;    // expresssion
+
+    public AssignmentStatement(String name, Expression expression, int lineNumber) {
+        super(lineNumber);
+
+        this.name = name;
+        this.expression = expression;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Expression getExpression() {
+        return expression;
+    }
+
+    @Override
+    public String toString() {
+        return name + " = " + expression.toString();
+    }
+}
