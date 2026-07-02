ddb7c42456345ea78781a21d8f6348eba5fd5407
Zubair <zubairabdulmatin@gmail.com>
2026-07-02 11:21:53 +0100

    Completing variable declaration statement node class

--- compiler/src/zplusplus/ast/VariableDeclarationStatement.java
+++ compiler/src/zplusplus/ast/VariableDeclarationStatement.java
@@ -1,4 +1,32 @@
 package zplusplus.ast;
 
-public class VariableDeclarationStatement {
+/**
+ * Class to handle variable declarations
+ *
+ * @author Zubair Abdul Matin
+ */
+public class VariableDeclarationStatement extends Statement {
+
+    private final String typeName;
+    private final String varName;
+
+    public VariableDeclarationStatement(String typeName, String varName, int lineNumber) {
+        super(lineNumber);
+
+        this.typeName = typeName;
+        this.varName = varName;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public String getVarName() {
+        return varName;
+    }
+
+    @Override
+    public String toString() {
+        return typeName + " " + varName + ";";
+    }
 }
