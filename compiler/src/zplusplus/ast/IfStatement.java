014b7a86843dbca85e5b055da52e015fff505b82
Zubair <zubairabdulmatin@gmail.com>
2026-07-02 11:46:40 +0100

    Completed if statement node, includes condition, if branch and else branch for now. if branch and else branch are considered statements, either singular ones or most likely block statements

--- compiler/src/zplusplus/ast/IfStatement.java
+++ compiler/src/zplusplus/ast/IfStatement.java
@@ -1,4 +1,45 @@
 package zplusplus.ast;
 
-public class IfStatement {
+/**
+ * Class for If statements (condition, if branch and else branch)
+ *
+ * @author Zubair Abdul Matin
+ */
+public class IfStatement extends Statement {
+
+    private final Expression condition;
+
+    // will be block statements
+    private final Statement ifBranch;
+    private final Statement elseBranch;
+
+    public IfStatement(Expression condition, Statement ifBranch, Statement elseBranch, int lineNumber) {
+        super(lineNumber);
+
+        this.condition = condition;
+        this.ifBranch = ifBranch;
+        this.elseBranch = elseBranch;
+    }
+
+    public Expression getCondition() {
+        return condition;
+    }
+
+    public Statement getIfStatement() {
+        return ifBranch;
+    }
+
+    public Statement getElseStatement() {
+        return elseBranch;
+    }
+
+    @Override
+    public String toString() {
+
+        if (elseBranch != null) {
+            return "if (" + condition.toString() + ") " + ifBranch.toString() + " else " + elseBranch.toString();
+        }
+
+        return "if (" + condition.toString() + ") " + ifBranch.toString();
+    }
 }
