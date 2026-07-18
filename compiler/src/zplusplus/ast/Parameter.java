ac964755b4f1b349ced17de26263f6127e2d0cdf
Zubair <zubairabdulmatin@gmail.com>
2026-07-18 17:03:38 +0100

    Adding a record class to hold function parameters

---
+++ compiler/src/zplusplus/ast/Parameter.java
@@ -0,0 +1,12 @@
+package zplusplus.ast;
+
+/**
+ * Holds the type and identifier of a function paramter
+ * @param type data type
+ * @param name paramter name
+ *
+ * @author Zubair Abdul Matin
+ */
+public record Parameter(String type, String name) {
+
+}
