f36a2bb0a882564cb3b64117b1f447268120fb1b
Zubair <zubairabdulmatin@gmail.com>
2026-07-20 17:24:13 +0100

    Changing the symbol record into a class so it can be inherited by specific variable and function symbol class (and possibly in the future an OOP one)

---
+++ compiler/src/zplusplus/sem_analysis/symbol/Symbol.java
@@ -0,0 +1,43 @@
+package zplusplus.sem_analysis.symbol;
+
+/**
+ * Generic Symbol class to hold the name of the
+ * symbol (variable/function) along with their type/return type.
+ */
+public abstract class Symbol {
+    private String name;
+    private String type;
+
+    /**
+     * Constructor for Symbol class
+     * @param name name of symbol
+     * @param type type of symbol
+     */
+    public Symbol(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    /**
+     * Gets name
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets type
+     * @return type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * To string method for the symbol
+     * @return symbol formatted as a string
+     */
+    @Override
+    abstract public String toString();
+}
