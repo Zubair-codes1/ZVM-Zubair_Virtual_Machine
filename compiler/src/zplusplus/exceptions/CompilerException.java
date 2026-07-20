56db73294ec6911f50168520bb3619a89f13088a
Zubair <zubairabdulmatin@gmail.com>
2026-07-20 17:53:22 +0100

    Updating Compiler exception class

--- compiler/src/zplusplus/exceptions/CompilerException.java
+++ compiler/src/zplusplus/exceptions/CompilerException.java
@@ -6,11 +6,22 @@ package zplusplus.exceptions;
  * @author Zubair Abdul Matin
  */
 public class CompilerException extends RuntimeException {
+    private String message;
+    private int lineNumber;
+
     /**
      * Passes all compiler execeptions to the Runtime execption
      * @param message error message
      */
-    public CompilerException(String message) {
-        super(message);
+    public CompilerException(String message, int lineNumber) {
+        super(message + " at line " + lineNumber);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
     }
 }
