a53b6a7f57bb1bc608d15096d546ab967ef008c0
Zubair <zubairabdulmatin@gmail.com>
2026-07-20 18:00:17 +0100

    Code generation exception class

--- compiler/src/zplusplus/exceptions/CodeGenException.java
+++ compiler/src/zplusplus/exceptions/CodeGenException.java
@@ -1,7 +1,12 @@
 package zplusplus.exceptions;
 
-public class CodeGenException extends RuntimeException {
-  public CodeGenException(String message) {
-    super(message);
-  }
+/**
+ * Code generation exception
+ *
+ * @author Zubair Abdul Matin
+ */
+public class CodeGenException extends CompilerException {
+    public CodeGenException(String message, int lineNumber) {
+        super(message, lineNumber);
+    }
 }
