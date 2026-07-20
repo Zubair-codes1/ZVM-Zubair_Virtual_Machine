70461bc5dd635c13bb03bc68f53de5d428ec7726
Zubair <zubairabdulmatin@gmail.com>
2026-07-20 16:39:55 +0100

    Created class to hold symbols (information about variables and functions)

---
+++ compiler/src/zplusplus/sem_analysis/Symbol.java
@@ -0,0 +1,12 @@
+package zplusplus.sem_analysis;
+
+/**
+ * Class to hold symbols (variables/functions and their types)
+ * @param name name of variable/function
+ * @param type type of variable/function
+ * @param isFunc boolean value to check if it is a function
+ *
+ * @author Zubair Abdul Matin
+ */
+public record Symbol(String name, String type, boolean isFunc) {
+}
