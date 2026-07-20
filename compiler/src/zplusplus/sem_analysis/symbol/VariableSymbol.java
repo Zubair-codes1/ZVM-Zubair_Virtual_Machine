0c2bced8c1984f2fdf639d4ccf42f11c0277a8c5
Zubair <zubairabdulmatin@gmail.com>
2026-07-20 17:32:45 +0100

    Variable Symbol implementation

--- compiler/src/zplusplus/sem_analysis/symbol/VariableSymbol.java
+++ compiler/src/zplusplus/sem_analysis/symbol/VariableSymbol.java
@@ -1,4 +1,27 @@
 package zplusplus.sem_analysis.symbol;
 
-public class VariableSymbol {
+/**
+ * Variable symbol class
+ *
+ * @author Zubair Abdul Matin
+ */
+public class VariableSymbol extends Symbol {
+
+    /**
+     * Variable Symbol constructor
+     * @param name variable name
+     * @param type variable type
+     */
+    public VariableSymbol(String name, String type) {
+        super(name, type);
+    }
+
+    /**
+     * Displays variable as a string
+     * @return variable as a string
+     */
+    @Override
+    public String toString() {
+        return getType() + " " + getName();
+    }
 }
